11477dc9461e542c8f08d8720b5e9308a6111be8
wjona13 <william.ona@revature.com>
2022-08-17 00:03:44 -0400

    Added application code and readme instructions

---
+++ Application.java
@@ -0,0 +1,48 @@
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.Map.Entry;
+
+public class Application{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a string for the program to count");
+        String input = scan.nextLine();
+        wordCounter(input);
+
+    }
+
+    private static void wordCounter(String input) {
+        //initialize hashmap to keep track of counts
+        Map<String,Integer> words = new HashMap<>();
+
+        //use scanner to parse through input string and populate HashMap
+        Scanner scan = new Scanner(input);
+        while(scan.hasNext()){
+            String next = scan.next();
+            next = next.toLowerCase();
+            //add next string to map if it is a string that only contains characters
+            if(containsOnlyLetters(next)) words.put(next, words.getOrDefault(next, 0)+1);
+        }
+        scan.close();
+
+        //order the items by frequency in a priority queue
+        PriorityQueue<Entry<String,Integer>> pq = new PriorityQueue<>((a,b) -> b.getValue()-a.getValue());
+        words.entrySet().stream().forEach(e-> pq.add(e));
+        System.out.println(pq);
+
+        //print out each value and key
+        while(!pq.isEmpty()){
+            Entry<String,Integer> current = pq.poll();
+            System.out.println(current.getValue()+ " " + current.getKey());
+        }
+    }
+
+    // returns true if input string only continas letters
+    private static boolean containsOnlyLetters(String input){
+        //uses an IntStream from input to check to see if each character is a letter
+        return input.chars().allMatch(Character::isLetter);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # WordFrequency
+Write a Java program to calculate word frequency.
+Given a piece of text, it should output each word in the text and the number of times it occurred.
+The words should be sorted by frequency, in descending order. Words with the same frequency
+can be in any order.
+For example for the input “the cat is in the bag”, output should be:
+2 the
+1 cat
+1 is
+1 in
+1 bag
+
+Please share your solution via a github
