61360c89fbe71874a088af864638d867030cf29d
wjona13 <william.ona@revature.com>
2022-08-17 00:18:17 -0400

    Added application code and readme instructions

--- Application.java
+++ Application.java
@@ -17,7 +17,20 @@ public class Application{
     private static void wordCounter(String input) {
         //initialize hashmap to keep track of counts
         Map<String,Integer> words = new HashMap<>();
+        populateWords(input, words);
 
+        //order the items by frequency in a priority queue
+        PriorityQueue<Entry<String,Integer>> pq = new PriorityQueue<>((a,b) -> b.getValue()-a.getValue());
+        words.entrySet().stream().forEach(e-> pq.add(e));
+
+        //print out each value and key
+        while(!pq.isEmpty()){
+            Entry<String,Integer> current = pq.poll();
+            System.out.println(current.getValue()+ " " + current.getKey());
+        }
+    }
+
+    private static void populateWords(String input, Map<String, Integer> words) {
         //use scanner to parse through input string and populate HashMap
         Scanner scan = new Scanner(input);
         while(scan.hasNext()){
@@ -27,17 +40,6 @@ public class Application{
             if(containsOnlyLetters(next)) words.put(next, words.getOrDefault(next, 0)+1);
         }
         scan.close();
-
-        //order the items by frequency in a priority queue
-        PriorityQueue<Entry<String,Integer>> pq = new PriorityQueue<>((a,b) -> b.getValue()-a.getValue());
-        words.entrySet().stream().forEach(e-> pq.add(e));
-        System.out.println(pq);
-
-        //print out each value and key
-        while(!pq.isEmpty()){
-            Entry<String,Integer> current = pq.poll();
-            System.out.println(current.getValue()+ " " + current.getKey());
-        }
     }
 
     // returns true if input string only continas letters
--- README.md
+++ README.md
@@ -1,13 +1,36 @@
 # WordFrequency
+## Problem Statement
 Write a Java program to calculate word frequency.
 Given a piece of text, it should output each word in the text and the number of times it occurred.
 The words should be sorted by frequency, in descending order. Words with the same frequency
 can be in any order.
+
 For example for the input “the cat is in the bag”, output should be:
+```
 2 the
 1 cat
 1 is
 1 in
 1 bag
+```
 
 Please share your solution via a github
+
+## Prerequisites
+Please have at least Java 8 installed. Some of the syntax used in the code require at least Java 8.
+
+## How to Run
+The following steps are used to run the application with the least amount of additional tooling.
+
+1. Clone the project:
+    > git clone https://github.com/WilliamOna/WordFrequency.git
+1. compile the source code:
+    > javac Application.java
+1. The step above should generate a file called 'Application.class'. Run this file:
+    > java Application
+1. Follow the prompt and enter any string that you like
+
+## Considerations
+- The application will take the lowercase version of strings that the user inputs. The frequency count is not case sensitive, so "DOG" will match "dog"
+- Only words that consist of only letters will be counted. Other strings that are parsed will be ignored.
+- If no valid word is given in the input string. There will be no output to display
